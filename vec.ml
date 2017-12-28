e3826eef01f1ad55c90e7d0b89771c143e38a66b
wiatrak2 <wpratkowiecki@gmail.com>
2017-12-28 22:40:58 +0100

    add vec implementation

---
+++ vec.ml
@@ -0,0 +1,32 @@
+module type VEC3 =
+sig 
+	type t
+	type vec3
+	val make : t -> t -> t -> vec3
+	val get : vec3 -> t *  t * t
+	val add : vec3 -> vec3 -> vec3
+	val sub : vec3 -> vec3 -> vec3
+	val mul : vec3 -> vec3 -> vec3
+	val div : vec3 -> vec3 -> vec3
+	val smul : t -> vec3 -> vec3
+	val dot : vec3 -> vec3 -> t
+	val abs : vec -> t
+	val norm : vec -> vec
+end
+
+module Vec3f : VEC3 with type t = float = 
+struct
+	type t = float
+	type vec3 = { x:float; y:float; z:float }
+	let make x y z = { x = x; y = y; z = z }
+	let get vec = (vec.x, vec.y, vec.z)
+	let add v1 v2 = { x = v1.x +. v2.x ; y = v1.y +. v2.y ; z = v1.z +. v2.z }
+	let sub v1 v2 = { x = v1.x -. v2.x ; y = v1.y -. v2.y ; z = v1.z -. v2.z }
+	let mul v1 v2 = { x = v1.x *. v2.x ; y = v1.y *. v2.y ; z = v1.z *. v2.z }
+	let div v1 v2 = { x = v1.x /. v2.x ; y = v1.y /. v2.y ; z = v1.z /. v2.z }
+	let smul s v = { x = s *. v.x ; y = s *. v.y ; z = s *. v.z }
+	let dot v1 v2 = v1.x *. v2.x +. v1.y *. v2.y +. v1.z *. v2.z
+	let abs v = sqrt @@ dot v v
+	let norm v = let r = 1. /. abs v in 
+		{ x = r *. v.x ; y = r *. v.y ; z = r *. v.z }
+end
